fa5fbbdc0a2212d5d05aacd7de6e7d1a07ee10c5
mattaperkins <matt@perkins.id.au>
2025-05-30 17:36:16 +1000

    addded better info to alerta for high latency and also clears old latency alarms likely could be made prity

--- status.php
+++ status.php
@@ -2,12 +2,13 @@
 // Status php proxy to send status from Mikrotik to alerta
 // Matt Perkins May 2025 
 
-$url = 'http://192.168.0.2:8080/api/heartbeat';
-$minimum_rtt = 100000 ; //100ms 
+$url = 'http://192.168.0.2:8080/api/';
+$minimum_rtt = 250000 ; //250ms 
+$lockpath="/dev/shm"; 
 
 // API key header
 $headers = [
-    'Authorization: Key al-spec-key-1',
+    'Authorization: Key your-api-key',
     'Content-Type: application/json',
 ];
 
@@ -20,12 +21,132 @@ list($xx,$mikrotik_host,$link_type,$link_loss,$link_rtt) = explode ("/",$path_in
 
 echo "$mikrotik_host  $link_type  $link_loss  $link_rtt "; 
 
-// If we get a bad result either high rtt or not zero loss
-//
+
+// If we get a bad result either high rtt 
+$ms_link_rtt = $link_rtt/1000;
 if($link_rtt > $minimum_rtt){ 
-	echo "exit - high rtt\n";
-	exit();
+
+$fp = fopen("$lockpath/$mikrotik_host",'c');
+
+if(!$fp){
+	die("cold not open lock file");
 }
+
+fclose($fp); 
+
+// Build the JSON payload as an array
+$data = [
+    "attributes" => [
+        "region" => "AU"
+    ],
+    "correlate" => [
+        "HttpServerError",
+        "HttpServerOK"
+    ],
+    "environment" => "Production",
+    "event" => "LATENCY",
+    "group" => "RouterDB",
+    "origin" => "$mikrotik_host/$link_type",
+    "resource" => "$mikrotik_host/$link_type",
+    "service" => [
+        "RouterDB"
+    ],
+    "severity" => "minor",
+    "tags" => ["mikrotik_script"],
+    "text" => "Hight RTT time $ms_link_rtt ms",
+    "type" => "",
+    "value" => "$mikrotik_host/$link_type"
+];
+
+
+// Convert the array to JSON
+$jsonPayload = json_encode($data);
+
+// Setup the cURL request
+$ch = curl_init("$url/alert");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonPayload);
+
+// Execute and check for errors
+$response = curl_exec($ch);
+if ($response === false) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    echo 'Alarm Response: ' . $response;
+}
+
+curl_close($ch);
+
+}else{
+// Clear latency message
+	if (is_file("$lockpath/$mikrotik_host")) {
+
+// Build the JSON payload as an array
+$data = [
+    "attributes" => [
+        "region" => "AU"
+    ],
+    "correlate" => [
+        "HttpServerError",
+        "HttpServerOK"
+    ],
+    "environment" => "Production",
+    "event" => "LATENCY",
+    "group" => "RouterDB",
+    "origin" => "$mikrotik_host/$link_type",
+    "resource" => "$mikrotik_host/$link_type",
+    "service" => [
+        "RouterDB"
+    ],
+    "severity" => "cleared",
+    "tags" => ["mikrotik_script"],
+    "text" => "Hight RTT time $ms_link_rtt ms",
+    "type" => "",
+    "value" => "$mikrotik_host/$link_type"
+];
+
+
+// Convert the array to JSON
+$jsonPayload = json_encode($data);
+
+// Setup the cURL request
+$ch = curl_init("$url/alert");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonPayload);
+
+// Execute and check for errors
+$response = curl_exec($ch);
+if ($response === false) {
+    echo 'Curl error: ' . curl_error($ch);
+} else {
+    echo 'Alarm Response: ' . $response;
+}
+
+unlink("$lockpath/$mikrotik_host"); 
+
+} 
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
 
 if($link_loss == 0 ){
 
@@ -39,7 +160,7 @@ $data = [
         'environment' => 'Production',
         'service' => ['RouterDB'],
         'group' => 'Network',
-        'severity' => 'minor',
+        'severity' => 'indeterminate',
     ],
 ];
 
@@ -48,7 +169,7 @@ $jsonData = json_encode($data);
 
 
 // Initialise cURL
-$ch = curl_init($url);
+$ch = curl_init("$url/heartbeat");
 
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
@@ -63,13 +184,12 @@ $response = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'cURL error: ' . curl_error($ch);
 } else {
-   echo "exit - 100% loss"; 
+   echo 'Heartbeat Response: ' . $response;
 }
 
 // Close cURL handle
 curl_close($ch);
 
-
-
-
 }
+
+
